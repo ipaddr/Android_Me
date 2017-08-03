28414c889fdab1220e448b732d888ab54209a42a
i_p_addr@yahoo.com <i_p_addr@yahoo.com>
2017-08-03 15:04:29 +0700

    adding head part fragment

---
+++ app/src/main/java/com/example/android/android_me/BodyPartFragment.java
@@ -0,0 +1,28 @@
+package com.example.android.android_me;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BodyPartFragment extends Fragment {
+
+
+    public BodyPartFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_body_part, container, false);
+    }
+
+}
